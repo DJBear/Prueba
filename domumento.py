e1b0ec0a4318288776bc05b9abf60b6a0f15413f
unknown <djb3ar23@gmail.com>
2014-10-29 18:41:29 -0600

    python funcionando

--- domumento.py
+++ domumento.py
@@ -1,7 +1,64 @@
-__author__ " 'DJBear'
+__autor__ = 'DJBear'
+
+
 def sumaDos():
-    print 10 *20
-    def main():
-        if __name__ == "__main__":
-            main()
-            
+    print 10 + 20
+
+
+def division(a, b):
+    result = a / b
+    print result
+
+
+def cast():
+    t = 10
+    f = 10.5
+
+    int(10.5)
+    print
+
+
+class Estudiante(object):
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def hola(self):
+        return self.nombre
+
+    def esMayor(self):
+        if self.edad >= 18:
+            return True
+        else:
+            return False
+
+
+def Exception():
+    try:
+        3 / 0
+    except ZeroDivisionError as e:
+        print e
+
+
+def main():
+    sumaDos()
+    division(100.54, 2100.0)
+    e = Estudiante("jesus", 15)
+    print "hola %s" % e.hola()
+
+    if e.esMayor():
+        print"Es mayor de edad"
+    else:
+        print "Es menor de edad"
+
+    contador = 0
+    while contador <= 10:
+        print contador
+        contador += 1
+    Exception()
+
+
+if __name__ == "__main__":
+    main()
+ 
+ 
